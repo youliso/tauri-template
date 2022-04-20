fe98aa851e9da6a642fb25b24e7d0c7ef88b0f19
youliso <youliso@hotmail.com>
2022-04-20 17:49:35 +0800

    feat: init

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,10 @@
+use tauri_build::{try_build, Attributes, WindowsAttributes};
+
 fn main() {
-  tauri_build::build()
+  if let Err(error) = try_build(
+    Attributes::new()
+      .windows_attributes(WindowsAttributes::new().window_icon_path("../resources/build/icons/icon.ico")),
+  ) {
+    panic!("error found during tauri-build: {:#?}", error);
+  }
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,20 @@
-#![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
-)]
+use tauri::Manager;
 
 fn main() {
   tauri::Builder::default()
+    .setup(|app| {
+      let main_window = app.get_window("main").unwrap();
+      // we perform the initialization code on a new task so the app doesn't freeze
+      tauri::async_runtime::spawn(async move {
+        // initialize your app here instead of sleeping :)
+        println!("Initializing...");
+        std::thread::sleep(std::time::Duration::from_secs(2));
+        println!("Done initializing.");
+        
+        main_window.show().unwrap();
+      });
+      Ok(())
+    })
     .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    .expect("failed to run app");
 }
